07d821b2d6efb0f6d4dc499185710aa7fc3b259b
Ferreira <gabrielferreirape502@gmail.com>
2026-02-03 19:56:59 -0300

    adding node

---
+++ Back-End/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/teste', (req, res) => {
+    res.json({ msg: 'Backend funcionando' });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
